f295272bb75f35218489dd1a876ea14121431f5e
smounsaveng <saypraseuth.mounsaveng.1@etsmtl.net>
2018-05-07 19:48:09 -0400

    Added Spatial Transfer network

--- models/generator.py
+++ models/generator.py
@@ -3,7 +3,7 @@
 
 # python 3 confusing imports :(
 from .unet_parts import *
-
+import torch.nn.utils.weight_norm as weight_norm
 
 class UNet(nn.Module):
     def __init__(self, n_channels_in, n_channels_out, nz):
@@ -30,4 +30,67 @@ class UNet(nn.Module):
         x = self.up3(x, x2)
         x = self.up4(x, x1)
         x = self.outc(x)
+        return x
+
+class SCTG(nn.Module):
+    def __init__(self, isize, nc, nfilter, nz):
+        super(SCTG, self).__init__()
+        assert isize % 16 == 0, "isize has to be a multiple of 16"
+        self.nfilter = nfilter
+        n_filter_0 = int(nfilter * 0.75)
+        n_filter_1 = int(nfilter * 1.5)
+        n_filter_2 = nfilter * 3
+
+        # Spatial transformer localization-network
+        self.localization = nn.Sequential(
+            # input is nc x isize x isize
+            nn.Conv2d(nc, n_filter_1, 3, 1, 1, bias=False),
+            nn.LeakyReLU(0.2),
+            weight_norm(nn.Conv2d(n_filter_1, n_filter_1, 3, 2, 1, bias=False)),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(p=0.5),
+            weight_norm(nn.Conv2d(n_filter_1, n_filter_2, 3, 1, 1, bias=False)),
+            nn.LeakyReLU(0.2),
+            weight_norm(nn.Conv2d(n_filter_2, n_filter_2, 3, 1, 1, bias=False)),
+            nn.LeakyReLU(0.2),
+            weight_norm(nn.Conv2d(n_filter_2, n_filter_2, 3, 2, 1, bias=False)),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(p=0.5),
+            weight_norm(nn.Conv2d(n_filter_2, n_filter_2, 3, 1, 0, bias=False)),
+            nn.LeakyReLU(0.2),
+            weight_norm(nn.Conv2d(n_filter_2, n_filter_2, 1, 1, 0, bias=False)),
+            nn.LeakyReLU(0.2),
+            weight_norm(nn.Conv2d(n_filter_2, n_filter_2, 1, 1, 0, bias=False)),
+            nn.LeakyReLU(0.2)
+        )
+
+        # Regressor for the 3 * 2 affine matrix
+        self.fc_loc = nn.Sequential(
+            nn.Linear(192 + nz, 32),
+            nn.ReLU(True),
+            nn.Linear(32, 3 * 2)
+        )
+
+        # Initialize the weights/bias with identity transformation
+        self.fc_loc[2].weight.data.zero_()
+        self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+
+    # Spatial transformer network forward function
+    def stn(self, x, noise):
+        xs = self.localization(x)
+        xs = xs.mean(3).mean(2)
+        xs = xs.view(-1, 192)
+        xs = torch.cat([xs, noise.squeeze(-1).squeeze(-1)], dim=1)
+        theta = self.fc_loc(xs)
+        theta = theta.view(-1, 2, 3)
+
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
+
+        return x
+
+    def forward(self, x, noise):
+        # transform the input
+        x = self.stn(x, noise)
+
         return x
