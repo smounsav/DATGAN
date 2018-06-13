4c71d0dcca873b22418d1edc2af6bf478e6073ee
smounsaveng <saypraseuth.mounsaveng.1@etsmtl.net>
2018-06-13 14:13:37 -0400

    Add toOneHot function

--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
+import torch
+
 def logger(directory, filename, string):
-    f = open(directory + '/' + filename, 'a')
-    f.write(string + '\n')
-    f.close()
+    with open(directory + '/' + filename, 'a') as f:
+        f.write(string + '\n')
+
+def toOneHot(input, nclasses):
+    onehotlabelssupport = torch.zeros(input.size(0), nclasses).to(input.device)
+    onehottrainrealunlbllabels = onehotlabelssupport.scatter_(1, input.unsqueeze(1), 1)
+    return onehottrainrealunlbllabels
