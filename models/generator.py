240de8508a404e6938c6e46adc797b81fac6df35
smounsaveng <saypraseuth.mounsaveng.1@etsmtl.net>
2018-08-20 11:55:16 -0400

    Multi-GPU + adding of STN layer

--- models/generator.py
+++ models/generator.py
@@ -3,12 +3,15 @@
 
 # python 3 confusing imports :(
 from .unet_parts import *
+import torch.nn as nn
+import torch.nn.parallel
 import torch.nn.utils.weight_norm as weight_norm
 
 class UNet(nn.Module):
-    def __init__(self, n_channels_in, n_channels_out, nz):
+    def __init__(self, isize, n_channels_in, n_channels_out, nz, ngpu):
         super(UNet, self).__init__()
-        self.inc = inconv(n_channels_in, 64, nz)
+        self.ngpu = ngpu
+        self.inc = inconv(isize, n_channels_in, 64, nz)
         self.down1 = down(64, 128)
         self.down2 = down(128, 256)
         self.down3 = down(256, 512)
@@ -19,22 +22,46 @@ class UNet(nn.Module):
         self.up4 = up(128, 64)
         self.outc = outconv(64, n_channels_out)
 
+        # Regressor for the 3 * 2 affine matrix
+        self.fc_loc = nn.Sequential(
+            nn.Linear(1024, 32),
+            nn.ReLU(),
+            nn.Linear(32, 3 * 2)
+        )
+
+        # Initialize the weights/bias with identity transformation
+        self.fc_loc[2].weight.data.zero_()
+        self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+
     def forward(self, x, z):
         x1 = self.inc(x, z)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
+        xs = x5
+        xs = xs.mean(3).mean(2)
+        xs = xs.view(-1, 1024)
+        theta = self.fc_loc(xs)
+        theta = theta.view(-1, 2, 3)
+        grid = F.affine_grid(theta, x.size())
+        xstn = F.grid_sample(x, grid)
+        x1stn = self.inc(xstn, z)
+        x2stn = self.down1(x1stn)
+        x3stn = self.down2(x2stn)
+        x4stn = self.down3(x3stn)
+        x5stn = self.down4(x4stn)
+        x = self.up1(x5stn, x4stn)
+        x = self.up2(x, x3stn)
+        x = self.up3(x, x2stn)
+        x = self.up4(x, x1stn)
         x = self.outc(x)
         return x
 
 class SCTG(nn.Module):
-    def __init__(self, isize, nc, nfilter, nz):
+    def __init__(self, isize, nc, nfilter, nz, ngpu):
         super(SCTG, self).__init__()
+        self.ngpu = ngpu
         assert isize % 16 == 0, "isize has to be a multiple of 16"
         self.nfilter = nfilter
         n_filter_0 = int(nfilter * 0.75)
@@ -68,7 +95,7 @@ class SCTG(nn.Module):
 
         # Regressor for the 3 * 2 affine matrix
         self.fc_loc = nn.Sequential(
-            nn.Linear(192 + nz, 32),
+            nn.Linear(192, 32),
             nn.ReLU(),
             nn.Linear(32, 3 * 2)
         )
@@ -78,11 +105,11 @@ class SCTG(nn.Module):
         self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
 
     # Spatial transformer network forward function
-    def stn(self, x, noise):
+    def stn(self, x):
         xs = self.localization(x)
         xs = xs.mean(3).mean(2)
         xs = xs.view(-1, 192)
-        xs = torch.cat([xs, noise.squeeze(-1).squeeze(-1)], dim=1)
+        #xs = torch.cat([xs, noise.squeeze(-1).squeeze(-1)], dim=1)
         theta = self.fc_loc(xs)
         theta = theta.view(-1, 2, 3)
 
@@ -91,8 +118,10 @@ class SCTG(nn.Module):
 
         return x
 
-    def forward(self, x, noise):
+    def forward(self, x):
         # transform the input
-        x = self.stn(x, noise)
-
+        if isinstance(x, torch.cuda.FloatTensor) and self.ngpu > 1:
+            x = nn.parallel.data_parallel(self.stn, x, range(self.ngpu))
+        else:
+            x = self.stn(x)
         return x
